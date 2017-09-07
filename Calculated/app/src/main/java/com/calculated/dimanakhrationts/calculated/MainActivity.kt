86e032be04ff10e4dfb8653c9a5d62c53f45e26f
Dima <dirmidante@gmail.com>
2017-09-07 19:32:12 +0300

    initial commit

---
+++ Calculated/app/src/main/java/com/calculated/dimanakhrationts/calculated/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.calculated.dimanakhrationts.calculated
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+ class MainActivity:AppCompatActivity() {
+
+protected override fun onCreate(savedInstanceState:Bundle?) {
+super.onCreate(savedInstanceState)
+setContentView(R.layout.activity_main)
+}
+}
